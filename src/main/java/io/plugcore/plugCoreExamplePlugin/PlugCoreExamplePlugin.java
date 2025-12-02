48c09ac3c2c7de5a7789a15fd22e868a0552f566
Trenton Taylor <trenton.taylor.email@gmail.com>
2025-12-01 18:04:09 -0600

    1.1.2

--- src/main/java/io/plugcore/plugCoreExamplePlugin/PlugCoreExamplePlugin.java
+++ src/main/java/io/plugcore/plugCoreExamplePlugin/PlugCoreExamplePlugin.java
@@ -1,6 +1,6 @@
 package io.plugcore.plugCoreExamplePlugin;
 
-import io.plugcore.plugCore.api.PlugCoreAPI;
+import io.plugcore.plugCore.PlugCore;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlugCoreExamplePlugin extends JavaPlugin {
@@ -8,11 +8,8 @@ public final class PlugCoreExamplePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         try {
-            if (!PlugCoreAPI.requireAuthorization(this)) {
-                getServer().getPluginManager().disablePlugin(this);
-                return;
-            }
-            getLogger().info("PlugCoreExamplePlugin enabled and authorized!");
+            PlugCore.requireAuthorization(this);
+            getLogger().info("PlugCoreExamplePlugin enabled - authorization check scheduled.");
         } catch (NoClassDefFoundError e) {
             getLogger().severe("PlugCore not found! Download from https://plugcore.io");
             getServer().getPluginManager().disablePlugin(this);
